7619e143f8958fd00ba902a52ad87a52f346140c
TheViking733n <anant733n@gmail.com>
2022-02-04 10:20:18 +0530

    initial commit

---
+++ bot.py
@@ -0,0 +1,525 @@
+﻿# -*- coding: utf-8 -*-
+"""
+Created on Sun Nov 7 16:34:58 2021
+
+@author: ANANT PRAKASH SINGH
+@GitHub: https://github.com/TheViking733n
+
+"""
+
+import logging
+import os
+import telegram
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
+from telegram import ParseMode
+import threading
+import time
+
+from variables import *
+from all_cutoff import *
+
+no_of_branches_per_college = 3
+
+
+# Below dictionary stores the latest replies of all the user in format-
+# { (user_id, chat_id) : [user_msg] }
+# to take input from that user
+ALL_REPLIES = {}
+
+
+
+# Below dictionary stores True if a INPUT() is waiting from a user for input-
+# {(user_id, chat_id) : True }
+# /cancel commands sets False to it
+WAITING_FOR_INPUT = {}
+
+
+
+
+
+
+
+
+
+
+# Bot TOKEN initialisation
+TOKEN ="YOUR-BOT-TOKEN-HERE"
+APP_NAME = "YOUR-HEROKU-APP-LINK-HERE"
+
+
+# Important!!! bot_trigger_method = "polling" or "webhook"
+bot_trigger_method = "webhook"
+
+
+# Creating Bot object
+bot = telegram.Bot(token=TOKEN)
+PORT = int(os.environ.get('PORT', '8443'))
+
+
+# Enable logging
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',level=logging.INFO)
+logger = logging.getLogger(__name__)
+
+
+# Defining bot commands jo ham logo ka bot padhega aur reply karega
+
+def start(update, context):
+    # Starting start command in new thead
+    # I'm starting this command in new thread because it contains INPUT() which is blocking CommandHandeler to call further commands
+    thread1 = threading.Thread(target=start_in_newThread, args=(update, context))
+    thread1.start()
+
+def start_in_newThread(update, context):
+    uid = update.message.from_user.id
+    uname = update.message.from_user.username
+    cid = update.effective_chat.id
+    displayname = update.message.from_user.full_name
+
+    msg = f"""🎉🎉Welcome <a href="tg://user?id={uid}">{displayname}</a> to the help section of this bot🎉🎉.\n\nI will tell you about what colleges you can get according to your rank in JEE.\n\nPlease answer few questions about yourself!"""
+    context.bot.send_message(chat_id=cid, text=msg, parse_mode=ParseMode.HTML)
+
+    msg = f"<i>{list_to_index_msg(ALL_STATES)}</i>\n\n<b>Enter index of state from which you passed class 12th:</b>"
+    context.bot.send_message(chat_id=cid, text=msg, parse_mode=ParseMode.HTML)
+    user_input = INPUT(uid, cid)
+    state_index = parse_user_input(user_input)
+    if user_input == "TIME UP":
+        msg = "⚠Time Up...⚠\n\n\nYour current operation cancelled.\n\n Please use /start to restart the bot."
+        context.bot.send_message(chat_id=cid, text=msg)
+        return
+    elif user_input == None:
+        # It means user gave /cancel command
+        return
+    elif state_index not in range(32):
+        msg = "⚠Invalid Input...⚠\n\n\nYour current operation cancelled.\n\n Please use /start to restart the bot."
+        context.bot.send_message(chat_id=cid, text=msg)
+        return
+    state = ALL_STATES[state_index]
+
+    msg = f"<i>{list_to_index_msg(ALL_GENDERS)}</i>\n\n<b>Enter index of gender:</b>"
+    context.bot.send_message(chat_id=cid, text=msg, parse_mode=ParseMode.HTML)
+    user_input = INPUT(uid, cid)
+    gender_index = parse_user_input(user_input)
+    if user_input == "TIME UP":
+        msg = "⚠Time Up...⚠\n\n\nYour current operation cancelled.\n\n Please use /start to restart the bot."
+        context.bot.send_message(chat_id=cid, text=msg)
+        return
+    elif user_input == None:
+        # It means user gave /cancel command
+        return
+    elif gender_index not in range(3):
+        msg = "⚠Invalid Input...⚠\n\n\nYour current operation cancelled.\n\n Please use /start to restart the bot."
+        context.bot.send_message(chat_id=cid, text=msg)
+        return
+    gender = ALL_GENDERS[gender_index]
+    if gender == 'Female':
+        gender = 'Female-only'
+    else:
+        gender = 'Gender-Neutral'
+
+    msg = f"<i>{list_to_index_msg(ALL_CATEGORIES)}</i>\n\n<b>Enter index of your category:</b>"
+    context.bot.send_message(chat_id=cid, text=msg, parse_mode=ParseMode.HTML)
+    user_input = INPUT(uid, cid)
+    category_index = parse_user_input(user_input)
+    if user_input == "TIME UP":
+        msg = "⚠Time Up...⚠\n\n\nYour current operation cancelled.\n\n Please use /start to restart the bot."
+        context.bot.send_message(chat_id=cid, text=msg)
+        return
+    elif user_input == None:
+        # It means user gave /cancel command
+        return
+    elif category_index not in range(10):
+        msg = "⚠Invalid Input...⚠\n\n\nYour current operation cancelled.\n\n Please use /start to restart the bot."
+        context.bot.send_message(chat_id=cid, text=msg)
+        return
+    category = ALL_CATEGORIES[category_index]
+
+
+    # context.bot.send_message(chat_id=cid, text=f"{state}\n\n{gender}\n\n{category}")
+
+    msg = f"Enter your <b>JEE Mains <i>{category}</i></b> category rank: "
+    context.bot.send_message(chat_id=cid, text=msg, parse_mode=ParseMode.HTML)
+    user_input = INPUT(uid, cid)
+    jm_rank = parse_user_input(user_input)
+    if user_input == "TIME UP":
+        msg = "⚠Time Up...⚠\n\n\nYour current operation cancelled.\n\n Please use /start to restart the bot."
+        context.bot.send_message(chat_id=cid, text=msg)
+        return
+    elif user_input == None:
+        # It means user gave /cancel command
+        return
+    elif jm_rank not in range(1,10000001): # rank can't be greater than 1 crore
+        msg = "⚠Invalid Input...⚠\n\n\nYour current operation cancelled.\n\n Please use /start to restart the bot."
+        context.bot.send_message(chat_id=cid, text=msg)
+        return
+
+    msg = f"Enter your <b>JEE Advanced <i>{category}</i></b> category rank:\n<i>(if not qualified then enter -1)</i>"
+    context.bot.send_message(chat_id=cid, text=msg, parse_mode=ParseMode.HTML)
+    user_input = INPUT(uid, cid)
+    ja_rank = parse_user_input(user_input)
+    if user_input == "TIME UP":
+        msg = "⚠Time Up...⚠\n\n\nYour current operation cancelled.\n\n Please use /start to restart the bot."
+        context.bot.send_message(chat_id=cid, text=msg)
+        return
+    elif user_input == None:
+        # It means user gave /cancel command
+        return
+    elif ja_rank < 0:
+        # It means user didn't qualified in JEE Adv.
+        ja_rank = 10000000
+    elif ja_rank not in range(1,10000001): # rank can't be greater than 1 crore
+        msg = "⚠Invalid Input...⚠\n\n\nYour current operation cancelled.\n\n Please use /start to restart the bot."
+        context.bot.send_message(chat_id=cid, text=msg)
+        return
+
+    # context.bot.send_message(chat_id=cid, text=f"{state}\n\n{gender}\n\n{category}\n\n{jm_rank}\n\n{ja_rank}")
+
+
+    #####################################
+    # Job of taking input from user done!
+    #####################################
+
+    ######################################
+    # Now Finding eligible college of IITs
+    ######################################
+    eligible_colleges = {}
+    iit_branch_count = 0
+
+    for row in IIT_CUTOFF:
+        _college = row[0]
+        _branch = row[1]
+        _quota = row[2]
+        _category = row[3]
+        _gender = row[4]
+        _or = row[5]
+        _cr = row[6]
+
+        if _category == category and _gender == gender and _cr >= ja_rank:
+            iit_branch_count += 1
+            tup = (_cr, _branch)
+            if _college in eligible_colleges:
+                eligible_colleges[_college].append(tup)
+            else:
+                eligible_colleges[_college] = [tup]
+
+    if iit_branch_count > 0:
+        iit_msg = f"😉<u><b>Total {iit_branch_count} branches in {len(eligible_colleges)} different IIT colleges available for you.😉</b></u>\n\n"
+        iit_msg += f"<i>Displaying atmost top <b>{no_of_branches_per_college}</b> branches of each college</i>\n\n"
+        iit_msg_list = [] # Making a list so that message doesn't become too long
+        for college_name, branch_list in eligible_colleges.items():
+            branch_list.sort()
+            temp = f"😎<u><b>{college_name}</b></u>😎"
+            count = 1
+            for branch in branch_list:
+                temp += f"\n✅ <i>{branch[1]}</i>"
+                count += 1
+                if count > no_of_branches_per_college:
+                    break
+            iit_msg_list.append(temp)
+
+        if len(iit_msg_list) > 15:
+            msg1 = iit_msg + "\n\n\n".join(iit_msg_list[:15])
+            msg2 = "\n\n\n".join(iit_msg_list[15:])
+            context.bot.send_message(chat_id=cid, text=msg1, parse_mode=ParseMode.HTML)
+            context.bot.send_message(chat_id=cid, text=msg2, parse_mode=ParseMode.HTML)
+        
+    else:
+        iit_msg = "😢 <i>Sorry, you can't get any branch in IITs.</i>"
+        context.bot.send_message(chat_id=cid, text=iit_msg, parse_mode=ParseMode.HTML)
+
+
+    time.sleep(3)
+    ##################################
+    # Finding eligible college of NITs
+    ##################################
+    eligible_colleges = {}
+    nit_branch_count = 0
+
+    for row in NIT_CUTOFF:
+        _college = row[0]
+        _branch = row[1]
+        _quota = row[2]
+        _category = row[3]
+        _gender = row[4]
+        _or = row[5]
+        _cr = row[6]
+
+        # Checking state reservation quota for the student
+        if NIT_LOC[_college] == state:
+            quota = ['HS']
+        else:
+            quota = ['AI', 'OS']
+
+        if _category == category and _gender == gender and _quota in quota and _cr >= jm_rank:
+            nit_branch_count += 1
+            tup = (_cr, _branch)
+            if _college in eligible_colleges:
+                eligible_colleges[_college].append(tup)
+            else:
+                eligible_colleges[_college] = [tup]
+
+    if nit_branch_count > 0:
+        nit_msg = f"😉<u><b>Total {nit_branch_count} branches in {len(eligible_colleges)} different NIT colleges available for you.😉</b></u>\n\n"
+        nit_msg += f"<i>Displaying atmost top <b>{no_of_branches_per_college}</b> branches of each college</i>\n\n"
+        nit_msg_list = [] # Making a list so that message doesn't become too long
+        for college_name, branch_list in eligible_colleges.items():
+            branch_list.sort()
+            temp = f"😁<u><b>{college_name}</b></u>😁"
+            count = 1
+            for branch in branch_list:
+                temp += f"\n✅ <i>{branch[1]}</i>"
+                count += 1
+                if count > no_of_branches_per_college:
+                    break
+            nit_msg_list.append(temp)
+
+        if len(nit_msg_list) > 15:
+            msg1 = nit_msg + "\n\n\n".join(nit_msg_list[:15])
+            msg2 = "\n\n\n".join(nit_msg_list[15:])
+            context.bot.send_message(chat_id=cid, text=msg1, parse_mode=ParseMode.HTML)
+            context.bot.send_message(chat_id=cid, text=msg2, parse_mode=ParseMode.HTML)
+        
+    else:
+        nit_msg = "😢 <i>Sorry, you can't get any branch in NITs.</i>"
+        context.bot.send_message(chat_id=cid, text=nit_msg, parse_mode=ParseMode.HTML)
+
+
+
+    time.sleep(3)
+    ###################################
+    # Finding eligible college of IIITs
+    ###################################
+    eligible_colleges = {}
+    iiit_branch_count = 0
+
+    for row in IIIT_CUTOFF:
+        _college = row[0]
+        _branch = row[1]
+        _quota = row[2]
+        _category = row[3]
+        _gender = row[4]
+        _or = row[5]
+        _cr = row[6]
+
+        if _category == category and _gender == gender and _cr >= jm_rank:
+            iiit_branch_count += 1
+            tup = (_cr, _branch)
+            if _college in eligible_colleges:
+                eligible_colleges[_college].append(tup)
+            else:
+                eligible_colleges[_college] = [tup]
+
+    if iiit_branch_count > 0:
+        iiit_msg = f"😉<u><b>Total {iiit_branch_count} branches in {len(eligible_colleges)} different IIIT colleges available for you.😉</b></u>\n\n"
+        iiit_msg += f"<i>Displaying atmost top <b>{no_of_branches_per_college}</b> branches of each college</i>\n\n"
+        iiit_msg_list = [] # Making a list so that message doesn't become too long
+        for college_name, branch_list in eligible_colleges.items():
+            branch_list.sort()
+            temp = f"🙃<u><b>{college_name}</b></u>🙃"
+            count = 1
+            for branch in branch_list:
+                temp += f"\n✅ <i>{branch[1]}</i>"
+                count += 1
+                if count > no_of_branches_per_college:
+                    break
+            iiit_msg_list.append(temp)
+
+        if len(iiit_msg_list) > 15:
+            msg1 = iiit_msg + "\n\n\n".join(iiit_msg_list[:15])
+            msg2 = "\n\n\n".join(iiit_msg_list[15:])
+            context.bot.send_message(chat_id=cid, text=msg1, parse_mode=ParseMode.HTML)
+            context.bot.send_message(chat_id=cid, text=msg2, parse_mode=ParseMode.HTML)
+        
+    else:
+        iiit_msg = "😢 <i>Sorry, you can't get any branch in IIITs.</i>"
+        context.bot.send_message(chat_id=cid, text=iiit_msg, parse_mode=ParseMode.HTML)
+
+
+
+    time.sleep(3)
+    ###################################
+    # Finding eligible college of GFTIs
+    ###################################
+    eligible_colleges = {}
+    gfti_branch_count = 0
+
+    for row in GFTI_CUTOFF:
+        _college = row[0]
+        _branch = row[1]
+        _quota = row[2]
+        _category = row[3]
+        _gender = row[4]
+        _or = row[5]
+        _cr = row[6]
+
+        # Checking state reservation quota for the student
+        if GFTI_LOC[_college] == state:
+            quota = ['HS']
+        else:
+            quota = ['AI', 'OS']
+
+        if _category == category and _gender == gender and _quota in quota and _cr >= jm_rank:
+            gfti_branch_count += 1
+            tup = (_cr, _branch)
+            if _college in eligible_colleges:
+                eligible_colleges[_college].append(tup)
+            else:
+                eligible_colleges[_college] = [tup]
+
+    if gfti_branch_count > 0:
+        gfti_msg = f"😉<u><b>Total {gfti_branch_count} branches in {len(eligible_colleges)} different GFTI colleges available for you.</b></u>😉\n\n"
+        gfti_msg += f"<i>Displaying atmost top <b>{no_of_branches_per_college}</b> branches of each college</i>\n\n"
+        gfti_msg_list = [] # Making a list so that message doesn't become too long
+        for college_name, branch_list in eligible_colleges.items():
+            branch_list.sort()
+            temp = f"😐<u><b>{college_name}</b></u>😐"
+            count = 1
+            for branch in branch_list:
+                temp += f"\n✅ <i>{branch[1]}</i>"
+                count += 1
+                if count > no_of_branches_per_college:
+                    break
+            gfti_msg_list.append(temp)
+        if len(gfti_msg_list) > 15:
+            msg1 = gfti_msg + "\n\n\n".join(gfti_msg_list[:15])
+            msg2 = "\n\n\n".join(gfti_msg_list[15:])
+            context.bot.send_message(chat_id=cid, text=msg1, parse_mode=ParseMode.HTML)
+            context.bot.send_message(chat_id=cid, text=msg2, parse_mode=ParseMode.HTML)
+        
+    else:
+        gfti_msg = "😢 <i>Sorry, you can't get any branch in GFTIs.</i>"
+        context.bot.send_message(chat_id=cid, text=gfti_msg, parse_mode=ParseMode.HTML)
+
+
+    msg = "🙃Thanks for using me🙃\n\nHope to see you soon.\nUse /start to restart"
+    context.bot.send_message(chat_id=cid, text=msg)
+
+
+
+def help(update, context):
+    uid = update.message.from_user.id
+    uname = update.message.from_user.username
+    cid = update.effective_chat.id
+    displayname = update.message.from_user.full_name
+
+    context.bot.send_message(chat_id=cid, text=help_reply.format(uid, displayname), parse_mode=ParseMode.HTML)
+
+def about(update, context):
+    uid = update.message.from_user.id
+    uname = update.message.from_user.username
+    cid = update.effective_chat.id
+    displayname = update.message.from_user.full_name
+
+    context.bot.send_message(chat_id=cid, text=about_reply.format(uid, displayname), parse_mode=ParseMode.HTML)
+
+
+def cancel(update, context):
+    global WAITING_FOR_INPUT
+
+    uid = update.message.from_user.id
+    uname = update.message.from_user.username
+    cid = update.effective_chat.id
+
+    key = (uid, cid)
+    WAITING_FOR_INPUT[key] = False
+    msg = "⚠Your current operation cancelled.⚠\n\n Please use /start to restart the bot."
+    context.bot.send_message(chat_id=cid, text=msg)
+
+def noncommand(update, context):
+    global ALL_REPLIES
+
+    uid = update.message.from_user.id
+    uname = update.message.from_user.username
+    cid = update.effective_chat.id
+    user_input = update.message.text
+
+    key = (uid, cid)
+    ALL_REPLIES[key] = [user_input]
+    print(ALL_REPLIES)
+
+
+def error(update, context):
+    logger.warning('Update "%s" caused error "%s"', update, context.error)
+
+
+
+def INPUT(uid, cid):
+    global ALL_REPLIES
+    global WAITING_FOR_INPUT
+
+    # Deleting last input from user (if available)
+    key = (uid, cid)
+    if key in ALL_REPLIES:
+        temp = ALL_REPLIES.pop(key)
+    
+    WAITING_FOR_INPUT[key] = True
+
+    # Waiting for input from user for 2 minutes
+
+    for i in range(120):
+        time.sleep(1)
+        if key in ALL_REPLIES:
+            return ALL_REPLIES[key][0]
+        if WAITING_FOR_INPUT[key] == False:
+            return None
+    return "TIME UP"
+
+
+
+def parse_user_input(user_input):
+    try:
+        num = int(user_input)
+    except ValueError:
+        num = None
+    except TypeError:
+        num = None   
+
+    return num
+
+
+def list_to_index_msg(lst):
+    msg = ""
+    for i in range(len(lst)):
+        msg += f"{i} -> {lst[i]}\n"
+    return msg[:-1]
+
+
+
+
+
+
+def main():
+    # This function will start the Bot
+    # Creating Updater and dispatcher
+    updater = Updater(TOKEN, use_context=True)
+    dp = updater.dispatcher
+
+    # If user typed any command
+    dp.add_handler(CommandHandler("start", start))
+    dp.add_handler(CommandHandler("help", help))
+    dp.add_handler(CommandHandler("about", about))
+    dp.add_handler(CommandHandler("cancel", cancel))
+    
+    # If user typed any non-command message
+    dp.add_handler(MessageHandler(Filters.text & ~Filters.command, noncommand))
+
+    # log all errors
+    dp.add_error_handler(error)
+
+    # Start the Bot
+
+    if bot_trigger_method == "polling":
+        updater.start_polling()
+
+    elif bot_trigger_method == "webhook":
+        updater.start_webhook(listen="0.0.0.0", port=PORT, url_path=TOKEN)
+        updater.bot.set_webhook(APP_NAME + TOKEN)
+
+    else:
+        print("Invalid bot trigger method")
+        
+    # Bot will run until we press Ctrl-C or the process receives SIGINT ,SIGTERM or SIGABRT
+    updater.idle()
+
+
+if __name__ == '__main__':
+    main()
---
+++ variables.py
@@ -0,0 +1,56 @@
+ALL_CATEGORIES = ['OPEN', 'OPEN (PwD)', 'EWS', 'EWS (PwD)', 'OBC-NCL', 'OBC-NCL (PwD)', 'SC', 'SC (PwD)', 'ST', 'ST (PwD)']
+
+
+ALL_NIT = ['Dr. B R Ambedkar NIT, Jalandhar', 'Indian Institute of Engineering Science and Technology, Shibpur', 'Malaviya NIT Jaipur', 'Maulana Azad NIT Bhopal', 'Motilal Nehru NIT Allahabad', 'NIT Agartala', 'NIT Arunachal Pradesh', 'NIT Calicut', 'NIT Delhi', 'NIT Durgapur', 'NIT Goa', 'NIT Hamirpur', 'NIT Karnataka, Surathkal', 'NIT Meghalaya', 'NIT Nagaland', 'NIT Patna', 'NIT Puducherry', 'NIT Raipur', 'NIT Sikkim', 'NIT, Andhra Pradesh', 'NIT, Jamshedpur', 'NIT, Kurukshetra', 'NIT, Manipur', 'NIT, Mizoram', 'NIT, Rourkela', 'NIT, Silchar', 'NIT, Srinagar', 'NIT, Tiruchirappalli', 'NIT, Uttarakhand', 'NIT, Warangal', 'Sardar Vallabhbhai NIT, Surat', 'Visvesvaraya NIT, Nagpur']
+ALL_IIT = ['IIT (BHU) Varanasi', 'IIT (ISM) Dhanbad', 'IIT Bhilai', 'IIT Bhubaneswar', 'IIT Bombay', 'IIT Delhi', 'IIT Dharwad', 'IIT Gandhinagar', 'IIT Goa', 'IIT Guwahati', 'IIT Hyderabad', 'IIT Indore', 'IIT Jammu', 'IIT Jodhpur', 'IIT Kanpur', 'IIT Kharagpur', 'IIT Madras', 'IIT Mandi', 'IIT Palakkad', 'IIT Patna', 'IIT Roorkee', 'IIT Ropar', 'IIT Tirupati']
+ALL_IIIT = ['Atal Bihari Vajpayee IIIT & Management Gwalior', 'IIIT (IIIT) Nagpur', 'IIIT (IIIT) Pune', 'IIIT (IIIT) Ranchi', 'IIIT (IIIT), Sri City, Chittoor', 'IIIT (IIIT)Kota, Rajasthan', 'IIIT Bhagalpur', 'IIIT Bhopal', 'IIIT Design & Manufacturing Kurnool, Andhra Pradesh', 'IIIT Guwahati', 'IIIT Lucknow', 'IIIT Manipur', 'IIIT Srirangam, Tiruchirappalli', 'IIIT Surat', 'IIIT(IIIT) Dharwad', 'IIIT(IIIT) Kalyani, West Bengal', 'IIIT(IIIT) Kilohrad, Sonepat, Haryana', 'IIIT(IIIT) Kottayam', 'IIIT(IIIT) Una, Himachal Pradesh', 'IIIT(IIIT), Vadodara, Gujrat', 'IIIT, Agartala', 'IIIT, Allahabad', 'IIIT, Design & Manufacturing, Kancheepuram', 'IIIT, Raichur, Karnataka', 'IIIT, Vadodara International Campus Diu (IIITVICD)', 'Pt. Dwarka Prasad Mishra IIIT, Design & Manufacture Jabalpur']
+ALL_GFTI = ['Assam University, Silchar', 'Birla Institute of Technology, Mesra, Ranchi', 'Central University of Rajasthan, Rajasthan', 'Central institute of Technology Kokrajar, Assam', 'Ghani Khan Choudhary Institute of Engineering and Technology, Malda, West Bengal', 'Gurukula Kangri Vishwavidyalaya, Haridwar', 'HNB Garhwal University Srinagar (Garhwal)', 'Indian Institute of Carpet Technology, Bhadohi', 'Institute of Infrastructure, Technology, Research and Management-Ahmedabad', 'Institute of Technology, Guru Ghasidas Vishwavidyalaya (A Central University), Bilaspur, (C.G.)', 'International Institute of Information Technology, Bhubaneswar', 'International Institute of Information Technology, Naya Raipur', 'J.K. Institute of Applied Physics & Technology, Department of Electronics & Communication, University of Allahabad- Allahabad', 'Jawaharlal Nehru University, Delhi', 'Mizoram University, Aizawl', 'National Institute of Electronics and Information Technology, Aurangabad (Maharashtra)', 'National Institute of Food Technology Entrepreneurship and Management, Sonepat, Haryana', 'National Institute of Foundry & Forge Technology, Hatia, Ranchi', 'North Eastern Regional Institute of Science and Technology, Nirjuli-791109 (Itanagar),Arunachal Pradesh', 'Pondicherry Engineering College, Puducherry', 'Punjab Engineering College, Chandigarh', 'Sant Longowal Institute of Engineering and Technology', 'School of Engineering, Tezpur University, Napaam, Tezpur', 'School of Planning & Architecture, Bhopal', 'School of Planning & Architecture, New Delhi', 'School of Planning & Architecture: Vijayawada', 'Shri Mata Vaishno Devi University, Katra, Jammu & Kashmir', 'University of Hyderabad', 'lndian Institute of Food Processing Technology, Thanjavur, Tamil Naidu.']
+
+ALL_GENDERS = ['Male', 'Female', 'Other']
+
+ALL_STATES = ['Andhra Pradesh', 'Arunachal Pradesh', 'Assam', 'Bihar', 'Chandigarh', 'Chhattisgarh', 'Goa', 'Gujarat', 'Haryana', 'Jammu and Kashmir', 'Jharkhand', 'Karnataka', 'Kerla', 'Madhya Pradesh', 'Maharashtra', 'Manipur', 'Meghalaya', 'Mizoram', 'Nagaland', 'New Delhi', 'Odisha', 'Puducherry', 'Punjab', 'Rajasthan', 'Sikkim', 'Tamil Naidu', 'Telangana', 'Triupura', 'Uttar Pradesh', 'Uttarakhand', 'West Bengal', 'Other']
+
+
+NIT_LOC = {'Dr. B R Ambedkar NIT, Jalandhar': 'Punjab', 'Indian Institute of Engineering Science and Technology, Shibpur': 'West Bengal', 'Malaviya NIT Jaipur': 'Rajasthan', 'Maulana Azad NIT Bhopal': 'Madhya Pradesh', 'Motilal Nehru NIT Allahabad': 'Uttar Pradesh', 'NIT Agartala': 'Triupura', 'NIT Arunachal Pradesh': 'Arunachal Pradesh', 'NIT Calicut': 'Kerla', 'NIT Delhi': 'New Delhi', 'NIT Durgapur': 'West Bengal', 'NIT Goa': 'Goa', 'NIT Hamirpur': 'Uttar Pradesh', 'NIT Karnataka, Surathkal': 'Karnataka', 'NIT Meghalaya': 'Meghalaya', 'NIT Nagaland': 'Nagaland', 'NIT Patna': 'Bihar', 'NIT Puducherry': 'Puducherry', 'NIT Raipur': 'Chhattisgarh', 'NIT Sikkim': 'Sikkim', 'NIT, Andhra Pradesh': 'Andhra Pradesh', 'NIT, Jamshedpur': 'Jharkhand', 'NIT, Kurukshetra': 'Haryana', 'NIT, Manipur': 'Manipur', 'NIT, Mizoram': 'Mizoram', 'NIT, Rourkela': 'Odisha', 'NIT, Silchar': 'Assam', 'NIT, Srinagar': 'Jammu and Kashmir', 'NIT, Tiruchirappalli': 'Tamil Naidu', 'NIT, Uttarakhand': 'Uttarakhand', 'NIT, Warangal': 'Telangana', 'Sardar Vallabhbhai NIT, Surat': 'Gujarat', 'Visvesvaraya NIT, Nagpur': 'Maharashtra'}
+GFTI_LOC = {'Assam University, Silchar': 'Assam', 'Birla Institute of Technology, Mesra, Ranchi': 'Jharkhand', 'Central University of Rajasthan, Rajasthan': 'Rajasthan', 'Central institute of Technology Kokrajar, Assam': 'Assam', 'Ghani Khan Choudhary Institute of Engineering and Technology, Malda, West Bengal': 'West Bengal', 'Gurukula Kangri Vishwavidyalaya, Haridwar': 'Uttarakhand', 'HNB Garhwal University Srinagar (Garhwal)': 'Jammu and Kashmir', 'Indian Institute of Carpet Technology, Bhadohi': 'Uttar Pradesh', 'Institute of Infrastructure, Technology, Research and Management-Ahmedabad': 'Gujarat', 'Institute of Technology, Guru Ghasidas Vishwavidyalaya (A Central University), Bilaspur, (C.G.)': 'Chhattisgarh', 'International Institute of Information Technology, Bhubaneswar': 'Odisha', 'International Institute of Information Technology, Naya Raipur': 'Chhattisgarh', 'J.K. Institute of Applied Physics & Technology, Department of Electronics & Communication, University of Allahabad- Allahabad': 'Uttar Pradesh', 'Jawaharlal Nehru University, Delhi': 'New Delhi', 'Mizoram University, Aizawl': 'Mizoram', 'National Institute of Electronics and Information Technology, Aurangabad (Maharashtra)': 'Maharashtra', 'National Institute of Food Technology Entrepreneurship and Management, Sonepat, Haryana': 'Haryana', 'National Institute of Foundry & Forge Technology, Hatia, Ranchi': 'Jharkhand', 'North Eastern Regional Institute of Science and Technology, Nirjuli-791109 (Itanagar),Arunachal Pradesh': 'Arunachal Pradesh', 'Pondicherry Engineering College, Puducherry': 'Puducherry', 'Punjab Engineering College, Chandigarh': 'Chandigarh', 'Sant Longowal Institute of Engineering and Technology': 'Punjab', 'School of Engineering, Tezpur University, Napaam, Tezpur': 'Assam', 'School of Planning & Architecture, Bhopal': 'Madhya Pradesh', 'School of Planning & Architecture, New Delhi': 'New Delhi', 'School of Planning & Architecture: Vijayawada': 'Andhra Pradesh', 'Shri Mata Vaishno Devi University, Katra, Jammu & Kashmir': 'Jammu and Kashmir', 'University of Hyderabad': 'Telangana', 'lndian Institute of Food Processing Technology, Thanjavur, Tamil Naidu.': 'Tamil Naidu'}
+
+
+help_reply = """🎉🎉Welcome <a href="tg://user?id={}">{}</a> to the help section of this bot🎉🎉
+
+This bot🤖 will predict what colleges and branch you can get according to you rank in JEE Mains and JEE Advanced 😉
+
+
+How to use this bot🤖❓
+
+Send /start message to this bot and answer a few question, that's it😎
+
+
+List of available commands:
+
+/start - Starts the bot🤖
+
+/help - Displays this help message🧐
+
+/about - Displays information about this bot🤖
+
+/cancel - Cancels current operation😑
+
+
+😇Enjoy!!!😇"""
+
+
+
+
+about_reply = """🎉🎉Welcome <a href="tg://user?id={}">{}</a> to the about section of this bot🎉🎉
+
+This bot🤖 was designed, developed and programmed by
+😎@TheViking733n😎
+
+
+🗓Date programmed:
+9 November, 2020
+
+🚫Disclaimer:
+
+This bot🤖 may not be 100% accurate since cutoff changes every year.
+Niether the programmer nor telegram will be responsible if the predictions are not satisfactory or inaccurate."""
